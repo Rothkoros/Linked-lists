fd1a204696edab7e98b1e46987ae7b3c600b6f3a
Nicholas Dombrosky <ndombrosky@gmail.com>
2020-11-07 14:48:02 -0700

    first commit

---
+++ linked-list/linked-list.js
@@ -0,0 +1,46 @@
+class LinkedListNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+    insert(num) {
+        let newNode = new LinkedListNode(num)
+        if (!this.head) {
+            this.head == newNode
+        } else {
+
+            let node = this.head
+            while (node.next) {
+                node = node.next
+            }
+            node.next = newNode
+        }
+    }
+    remove(val) {
+        let node = this.head;
+        let previous = null;
+        while (node) {
+            if (node.data == val)
+                if (previous != null) {
+                    previous.next = node.next
+                } else {
+                    this.head = null;
+                }
+            return;
+        }
+        previous = node;
+        node = node.next;
+    }
+    find(val)
+}
+
+
+
+
+let node1 = new LinkedListNode(2)
